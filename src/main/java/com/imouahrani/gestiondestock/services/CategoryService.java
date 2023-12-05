e2de838ca0d7c27be1a4a3a61be23163cce731d4
Imene <imene.ouahrani@capgemini.com>
2023-12-05 16:01:25 +0100

    GDS-06 : développement des services

---
+++ src/main/java/com/imouahrani/gestiondestock/services/CategoryService.java
@@ -0,0 +1,18 @@
+package com.imouahrani.gestiondestock.services;
+
+import com.bouali.gestiondestock.dto.CategoryDto;
+import java.util.List;
+
+public interface CategoryService {
+
+  CategoryDto save(CategoryDto dto);
+
+  CategoryDto findById(Integer id);
+
+  CategoryDto findByCode(String code);
+
+  List<CategoryDto> findAll();
+
+  void delete(Integer id);
+
+}
---
+++ src/main/java/com/imouahrani/gestiondestock/services/ClientService.java
@@ -0,0 +1,17 @@
+package com.imouahrani.gestiondestock.services;
+
+import com.imouahrani.gestiondestock.dto.ClientDto;
+
+import java.util.List;
+
+public interface ClientService {
+
+  ClientDto save(ClientDto dto);
+
+  ClientDto findById(Integer id);
+
+  List<ClientDto> findAll();
+
+  void delete(Integer id);
+
+}
